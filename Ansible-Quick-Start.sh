5ac1533eedc6e7f7d2e416542d43a30ee5620061
teo1927 <50788371+teo1927@users.noreply.github.com>
2020-09-09 10:12:18 -0400

    Add files via upload

---
+++ Ansible-Quick-Start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+BASEDIR=$(dirname $0)
+ABSPATH=$(readlink -f $0) #Absolute Path Of where The Script Sits
+ABSDIR=$(dirname $ABSPATH) #Directory Where The Script Is Running From
+newdir="${ABSDIR}/Ansible-Projects/"
+file="Ansible-Quick-Start.zip"
+zipfile="$newdir$file"
+echo "$ABSDIR"
+echo "$newdir"
+yum -y install epel-release
+if [ $? -eq 0 ] ; then 
+  echo 'Extra Packages for Enterprise Linux Installed' ;
+  else echo 'Error Installing Extra Packages for Enterprise Linux';
+  exit ;
+  fi
+yum -y install ansible
+if [ $? -eq 0 ] ; then 
+  echo 'Ansible Installed' ;
+  else echo 'Error Installing Ansible';
+  exit ;
+  fi
+yum -y install unzip
+if [ $? -eq 0 ] ; then 
+  echo 'Unzip Installed' ;
+  else echo 'Error Installing Unzip';
+  exit ;
+  fi
+yum -y install git
+if [ $? -eq 0 ] ; then 
+  echo 'Git Installed' ; 
+  else echo 'Error Installing Git';
+  exit ;
+  fi
+git clone https://github.com/teo1927/Ansible-Quick-Start-Setup.git $newdir
+unzip $zipfile -d $newdir
+mkdir "${ABSDIR}/Ansible-Projects/.New-Project-Template"
+cp -R ${newdir}Ansible-Quick-Start/ ${newdir}.New-Project-Template/
+rm $zipfile
