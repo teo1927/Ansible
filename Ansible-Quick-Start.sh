88ed246ead732bf1e76c6840e1a30daa38950865
teo1927 <50788371+teo1927@users.noreply.github.com>
2020-09-23 12:52:10 -0400

    Update Ansible-Quick-Start.sh

--- Ansible-Quick-Start.sh
+++ Ansible-Quick-Start.sh
@@ -52,15 +52,16 @@ rm -rf "${newdir}Setup-Projects/"
 ansible-playbook -i /opt/Ansible-Projects/.Setup-Projects/Setup-Projects/Jenkins-Server/inventories/staging/hosts /opt/Ansible-Projects/.Setup-Projects/Setup-Projects/Jenkins-Server/site.yml
 ansible-playbook -i /opt/Ansible-Projects/.Setup-Projects/Setup-Projects/VS-Code-Server/inventories/staging/hosts /opt/Ansible-Projects/.Setup-Projects/Setup-Projects/VS-Code-Server/site.yml
 
-externalip="curl ifconfig.co"
-jenkinslogin="docker exec -it jenkins cat /var/jenkins_home/secrets/initialAdminPassword"
+externalip=$(curl ifconfig.co)
+jenkinslogin=$(docker exec -it jenkins cat /var/jenkins_home/secrets/initialAdminPassword)
 
-echo "Ansible Workspace Located At /opt/Ansible-Projects"
-
-echo "Access File System Via Virtual Studio Code At http://${externalip}:8443/?folder=/opt"
-
-echo "Jenkins Dashboard Available At http://${externalip}:8080"
-
-echo "Jenkins Initial Administrator Password: ${jenkinslogin}"
-
+echo "Ansible Workspace:" 
+echo "Located At /opt/Ansible-Projects"
+echo.
+echo "Virtual Studio Code:"
+echo "At http://${externalip}:8443/?folder=/opt"
+echo.
+echo "Jenkins Dashboard:"
+echo "http://${externalip}:8080"
+echo.
 echo "Installation Complete"
