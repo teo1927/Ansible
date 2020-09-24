87983f4562849939a69a0ab34144068b2183b50e
teo1927 <50788371+teo1927@users.noreply.github.com>
2020-09-24 00:39:40 -0400

    Update Ansible-Quick-Start.sh

--- Ansible-Quick-Start.sh
+++ Ansible-Quick-Start.sh
@@ -1,14 +1,51 @@
 #!/bin/bash
 ABSPATH=$(readlink -f "$0") #Absolute Path Of where The Script Sits
 ABSDIR=$(dirname "$ABSPATH") #Directory Where The Script Is Running From
+externalip=$(curl -s ifconfig.co)
 newdir="/opt/Ansible-Projects/"
 firstprojectdir="/opt/Ansible-Projects/My-Projects/New-Ansible-Project/"
 myprojectdir="/opt/Ansible-Projects/My-Projects/"
 file="Ansible-Projects.zip"
 zipfile="$newdir$file"
 
-echo 'Starting Installation Sequence...'
+echo ''
+echo 'Installation Options'
+echo '1. Express Installation'
+echo '2. Custom Installation'
+echo ''
 
+while true; do
+read -pr 'Choose Installation Method [1/2]:' choice
+  if [[ $choice -eq '1' ]]; then
+    jenkinschoice='y'
+    codeserverchoice='y'
+  elif [[ $choice -eq '2' ]]; then
+    while true; do
+    read -pr "Install Jenkins Services? This will be available at http://${externalip}:8080 [y/n]" jenkinschoice
+      if [[ $jenkinschoice == 'y' ]]; then
+        break
+      elif [[ $jenkinschoice == 'n' ]]; then
+        break
+      else
+        echo 'Invalid Entry: Please Provide y or n'
+      fi
+    done
+    while true; do
+    read -pr "Install Visual Studio Code Services? This will be available at http://${externalip}:8443/?folder=/opt [y/n]" codeserverchoice
+      if [[ $codeserverchoice == 'y' ]]; then
+        break
+      elif [[ $codeserverchoice == 'n' ]]; then
+        break
+      else
+        echo 'Invalid Entry: Please Provide y or n'
+      fi
+    done
+  else
+    echo 'Invalid Entry: Please Provide 1 or 2'
+  fi
+done
+
+echo 'Starting Installation Sequence...'
 yum -y install epel-release > /dev/null 2>&1
 if [ $? -eq 0 ] ; then 
   echo 'EPEL ................................ Installed' ;
@@ -61,22 +98,60 @@ rm -f "$zipfile" > /dev/null 2>&1
 rm -f "${ABSDIR}/Ansible-Quick-Start.sh" > /dev/null 2>&1
 rm -f "${newdir}/README.md" > /dev/null 2>&1
 rm -rf "${newdir}Setup-Projects/" > /dev/null 2>&1
-ansible-playbook -i /opt/Ansible-Projects/.Setup-Projects/Setup-Projects/Service-Setup/inventories/staging/hosts /opt/Ansible-Projects/.Setup-Projects/Setup-Projects/Service-Setup/site.yml --tags "docker,jenkins,code-server" 
 
-externalip=$(curl -s ifconfig.co)
-jenkinslogin=$(docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword)
-echo " "
-echo "+------------------------------------------+"
-echo "| Service Information"
-echo "|"
-echo "| Ansible Workspace:"
-echo "| /opt/Ansible-Projects"
-echo "|"
-echo "| Virtual Studio Code:"
-echo "| http://${externalip}:8443/?folder=/opt"
-echo "|"
-echo "| Jenkins Dashboard:"
-echo "| http://${externalip}:8080"
-echo "| Password: ${jenkinslogin}"
-echo "+------------------------------------------+"
-echo "Installation Complete"
+if [[ $jenkinschoice == 'y' ]] && [[ $codeserverchoice == 'y' ]]; then
+  ansible-playbook -i /opt/Ansible-Projects/.Setup-Projects/Setup-Projects/Service-Setup/inventories/staging/hosts /opt/Ansible-Projects/.Setup-Projects/Setup-Projects/Service-Setup/site.yml --tags "docker,jenkins,code-server";
+  jenkinslogin=$(docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword);
+  echo " ";
+  echo "+------------------------------------------+";
+  echo "| Service Information";
+  echo "|";
+  echo "| Ansible Workspace:";
+  echo "| /opt/Ansible-Projects";
+  echo "|";
+  echo "| Virtual Studio Code:";
+  echo "| http://${externalip}:8443/?folder=/opt";
+  echo "|";
+  echo "| Jenkins Dashboard:";
+  echo "| http://${externalip}:8080";
+  echo "| Password: ${jenkinslogin}";
+  echo "+------------------------------------------+";
+  echo "Installation Complete"
+elif [[ $jenkinschoice == 'y' ]] && [[ $codeserverchoice == 'n' ]]; then
+  ansible-playbook -i /opt/Ansible-Projects/.Setup-Projects/Setup-Projects/Service-Setup/inventories/staging/hosts /opt/Ansible-Projects/.Setup-Projects/Setup-Projects/Service-Setup/site.yml --tags "docker,jenkins";
+  jenkinslogin=$(docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword);
+  echo " ";
+  echo "+------------------------------------------+";
+  echo "| Service Information";
+  echo "|";
+  echo "| Ansible Workspace:";
+  echo "| /opt/Ansible-Projects";
+  echo "|";
+  echo "| Jenkins Dashboard:";
+  echo "| http://${externalip}:8080";
+  echo "| Password: ${jenkinslogin}";
+  echo "+------------------------------------------+";
+  echo "Installation Complete"
+elif [[ $jenkinschoice == 'n' ]] && [[ $codeserverchoice == 'y' ]]; then
+  ansible-playbook -i /opt/Ansible-Projects/.Setup-Projects/Setup-Projects/Service-Setup/inventories/staging/hosts /opt/Ansible-Projects/.Setup-Projects/Setup-Projects/Service-Setup/site.yml --tags "docker,code-server";
+  echo " ";
+  echo "+------------------------------------------+";
+  echo "| Service Information";
+  echo "|";
+  echo "| Ansible Workspace:";
+  echo "| /opt/Ansible-Projects";
+  echo "|";
+  echo "| Virtual Studio Code:";
+  echo "| http://${externalip}:8443/?folder=/opt";
+  echo "+------------------------------------------+";
+  echo "Installation Complete"
+elif [[ $jenkinschoice == 'n' ]] && [[ $codeserverchoice == 'n' ]]; then
+  echo " ";
+  echo "+------------------------------------------+";
+  echo "| Service Information";
+  echo "|";
+  echo "| Ansible Workspace:";
+  echo "| /opt/Ansible-Projects";
+  echo "+------------------------------------------+";
+  echo "Installation Complete"
+fi
